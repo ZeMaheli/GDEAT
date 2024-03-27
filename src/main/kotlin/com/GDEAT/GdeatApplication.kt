9ac194ec2d494bfb21ec6b2ca700d79e1f2f8761
ZeMaheli <josemcv99@hotmail.com>
2024-03-27 16:36:51 +0000

    Web app beginning
    Commited beggining files of web app

---
+++ src/main/kotlin/com/GDEAT/GdeatApplication.kt
@@ -0,0 +1,11 @@
+package com.GDEAT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GdeatApplication
+
+fun main(args: Array<String>) {
+	runApplication<GdeatApplication>(*args)
+}
---
+++ src/test/kotlin/com/GDEAT/GdeatApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.GDEAT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GdeatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
